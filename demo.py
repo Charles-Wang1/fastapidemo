a69beec50a23e3f832b7405304c7b594777c92fc
wangc35 <charles.wang@vanderbilt.edu>
2022-04-17 21:23:42 -0500

    Sample code

---
+++ demo.py
@@ -0,0 +1,36 @@
+#Imports go here
+
+
+#Schemas go here 
+
+
+#@app.get("/")
+#async def root():
+ #enter code here
+
+
+#Check docs here
+ 
+
+#Methods
+#@app.get("/api/v1/users")
+#async def fetch_users():
+    #enter code here
+ 
+#@app.get("/api/v1/by-roles/{role_name}")
+#async def by_role_name(role_name: Role):
+   #enter code here
+ 
+ 
+#@app.post("/api/v1/user")
+#async def create_user(user: User):
+    #enter code here
+ 
+#@app.delete("/api/v1/user")
+#async def delete_user(user_id: UUID):
+   #enter code here
+ 
+#@app.put("/api/v1/users/{user_id}")
+#async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+  #enter code here
+          
---
+++ models.py
@@ -0,0 +1,13 @@
+#Imports
+
+
+#class Role(str, Enum):
+#enter code here
+
+
+#class User(BaseModel):
+#enter code here
+
+
+#class UserUpdateRequest(BaseModel):
+#enter code here
