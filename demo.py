356766ec5f1f4710ed28d857a215f26316e9d521
wangc35 <charles.wang@vanderbilt.edu>
2022-04-18 00:36:14 -0500

    change comments

--- demo.py
+++ demo.py
@@ -1,28 +1,30 @@
+#Try the following command on terminal:
 #pip install fastapi
+# if that doesn't work, try the following commands in this order:
 #pip3 install fastapi
 #python -m pip install fastapi
 #python3 -m pip install fastapi
 
+#Do the same thing for uvicorn 
 #pip install uvicorn
 #pip3 install uvicorn
 #python -m pip install uvicorn
 #python3 -m pip install uvicorn
 
+
+
 #Imports go here
 
 
-#Schemas go here 
+#Data goes here 
 
 
+#Endpoints
+ 
 #@app.get("/")
 #async def root():
  #enter code here
-
-
-#Check docs here
  
-
-#Methods
 #@app.get("/api/v1/users")
 #async def fetch_users():
     #enter code here
